c2d5df8a307b15bea6556788eb43cc0daf412056
Sriram Koritala <sriramk3@illinois.edu>
2026-08-07 20:06:53 -0500

    Add: combination-sum-iv - submission-0

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [1] + [0] * target
+        for i in range(1,target+1):
+            for num in nums:
+                if i < num:
+                    continue
+                dp[i] += dp[i - num]
+        
+        return dp[target]
+                
